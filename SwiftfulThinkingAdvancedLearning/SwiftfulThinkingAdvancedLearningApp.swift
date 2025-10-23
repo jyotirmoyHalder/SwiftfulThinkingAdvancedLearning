cbae1d4d7b5552628af4c4318d9f916cf0503fa1
Jyotirmoy Halder <jyotirmoy.cse5.bu@gmail.com>
2025-10-23 16:36:56 +0600

    Initial Commit

---
+++ SwiftfulThinkingAdvancedLearning/SwiftfulThinkingAdvancedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingAdvancedLearningApp.swift
+//  SwiftfulThinkingAdvancedLearning
+//
+//  Created by jyotirmoy_halder on 23/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingAdvancedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
